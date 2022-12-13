ef02e442e470de15dc2d382fade7cd2c9590679c
Ryan Munz <ryan.munzy@gmail.com>
2022-12-12 19:18:24 -0500

    commit tree 89e720627db32430fd5ae4c1fa5f24be8b86d106

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';}
+    else if (feet <= 2000)
+    return 'That will be twenty bucks.';
+    else if (feet > 2500)
+    return 'No can do.';
+    else if (feet > 2000)
+    return 'I will gladly take your thirty bucks.';
+   
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+ if (city === "NYC") {
+ return 'Ok, sounds good.';}
+else {return 'No go.';}
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';} 
+    else { return 'Bye.';} 
   // Write your code here!
 }
